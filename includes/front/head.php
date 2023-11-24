d233fc14a115dfcce1036fb8c403a74766109e04
Andrés Olguín Dev <acos2014600836@gmail.com>
2023-11-23 23:28:33 -0300

    a

--- includes/front/head.php
+++ includes/front/head.php
@@ -2,7 +2,10 @@
 <?php
 
     function u_head() {
+        ?>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
+        <?php
 
-        
     }
